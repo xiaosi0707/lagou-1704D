a8545ef0eae35140b551caca59f946d81dd6f2c9
wyunfei <583514368@qq.com>
2018-01-11 15:11:32 +0800

    简历投递 cookie

--- src/script/config/interceptor.js
+++ src/script/config/interceptor.js
@@ -1,23 +1,25 @@
 'use strict';
 
-angular.module('app').factory('http', function ($q, $rootScope, $window) {
+angular.module('app').factory('http', function ($q, $rootScope, $cookies) {
     return {
+        // 可选， 拦截成功的请求
         request: function (config) {
-            console.log('请求成功', config);
-            $rootScope.isLoading = true;
+            // console.log($cookies.get('name'));
+            $rootScope.isLoading = true;    // loading的实现 - 显示
             return config || $q.when(config);
         },
+        // 可选， 拦截失败的请求
         requestError: function (rejection) {
-            console.log('请求失败');
             return $q.reject(rejection)
         },
+        //可选， 拦截成功的响应
         response: function (response) {
-            console.log('响应成功', response.config.isLoading);
-            $rootScope.isLoading = false;
+            $rootScope.isLoading = false; // loading的实现 - 隐藏
             return response || $q.when(response);
         },
+        // 可选， 拦截失败的响应
         responseError: function (rejection) {
-            console.log('响应失败', rejection);
+            // 网站报错处理的实现
             switch (rejection.status) {
                 case 404:
                     alert('请求地址找不到');
@@ -30,7 +32,7 @@ angular.module('app').factory('http', function ($q, $rootScope, $window) {
         }
     }
 });
-
+// 将创建的拦截器加入到$httpProvider.interceptors数组中
 angular.module('app').config(function ($httpProvider) {
     $httpProvider.interceptors.push('http');
-})
+});
--- src/script/controller/position-info.js
+++ src/script/controller/position-info.js
@@ -1,15 +1,29 @@
 'use strict';
 angular.module('app').controller('positionInfo', function ($scope, $http, $state, $cookies) {
-
+    var id = $state.params.id;
+    var arrSend = [];
     $http.get('https://www.easy-mock.com/mock/5a52256ad408383e0e3868d7/lagou/position-detail').then(function (res) {
-        var id = $state.params.id;
+
         $scope.positionInfo = res.data[id];
         if ($cookies.get('id')) $scope.isLogin = true;
-
     });
     $scope.isSend = false;
+
+
+
+    // 容错处理
+    if ($cookies.get('isSend')) arrSend = $cookies.get('isSend').split(',');
+
+    //判断当前职位是否已经投递过
+    angular.forEach(arrSend, function (positionId) {
+        if (positionId === id) { $scope.isSend = true; }
+    });
+
+    // 简历投递
     $scope.send = function () {
-        alert('投递成功');
+        arrSend.push(id);
+        $cookies.put("isSend",arrSend);
         $scope.isSend = true;
+        alert('投递成功');
     }
 });
